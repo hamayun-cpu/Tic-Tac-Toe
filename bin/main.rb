d4280a4f0ae0283dd43eaaf5fe24a98f493a7989
hamayun-cpu <hamayun.waheed1999@gmail.com>
2020-10-16 04:04:15 +0500

    Now the board updates after every move and validates the name

--- bin/main.rb
+++ bin/main.rb
@@ -2,19 +2,26 @@
 
 puts "\t\t\t\tHello to Tic Tac Toe game Build by Hamayun and Asad!"
 puts "\n"
-puts 'Hey player-1, please enter your name'
-ply_one = gets.chomp
+dif = false
 
-puts 'Hey player-2, please enter your name'
-ply_two = gets.chomp
+while dif == false
+  puts 'Hey player-1, please enter your name'
+  ply_one = gets.chomp
 
-def printt
+  puts 'Hey player-2, please enter your name'
+  ply_two = gets.chomp
+
+  dif = true if !ply_one.empty? && !ply_two.empty? && ply_one != ply_two
+  puts 'Wrong Input' if dif == false
+end
+
+def printt(arr)
   puts "\t\t\t******************************"
-  puts "\t\t\t\t 0 | 1 | 2"
+  puts "\t\t\t\t #{arr[0]} | #{arr[1]} | #{arr[2]}"
   puts "\t\t\t\t-----------"
-  puts "\t\t\t\t 3 | 4 | 5"
+  puts "\t\t\t\t #{arr[3]} | #{arr[4]} | #{arr[5]}"
   puts "\t\t\t\t-----------"
-  puts "\t\t\t\t 6 | 7 | 8"
+  puts "\t\t\t\t #{arr[6]} | #{arr[7]} | #{arr[8]}"
   puts "\t\t\t******************************"
 end
 
@@ -22,9 +29,9 @@ puts "\n"
 puts "#{ply_one}, you have got X"
 puts "#{ply_two}, you have got O"
 puts "\n"
-puts '****Gameboard is displayed****'
-
+board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 puts 'NOTE: The player who wants to mark the box, just write its box number.'
+printt(board)
 
 puts "#{ply_one}, you got the first turn"
 
@@ -47,11 +54,13 @@ while game_on
   if avail_moves.include?(box)
     ans = avail_moves.index(box)
     avail_moves[ans] = -1
+    board[ans] = 'X' if i.even?
+    board[ans] = 'O' unless i.even?
     i += 1
   else
     puts 'Invalid Move'
   end
-  printt
+  printt(board)
 
   if i > 8
     puts "#{ply_two} WON"
