1eca8e699f793f84057005619e868c89f3cd4ee5
hamayun-cpu <hamayun.waheed1999@gmail.com>
2020-10-19 22:21:56 +0500

    added classes for player and game

---
+++ lib/logic.rb
@@ -0,0 +1,106 @@
+# Class of Game
+class Game
+  def initialize
+    @board = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    @avail_moves = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  end
+
+  def available_moves
+    @avail_moves.each do |num|
+      print "  #{num}" if num >= 0 && num < 9
+    end
+  end
+
+  def move(box_num, player)
+    if @avail_moves.include?(box_num)
+      ans = @avail_moves.index(box_num)
+      @avail_moves[ans] = -1
+      mark(box_num, player)
+      true
+    else
+      false
+    end
+  end
+
+  def display
+    puts "\t\t\t******************************"
+    puts "\t\t\t\t #{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "\t\t\t\t-----------"
+    puts "\t\t\t\t #{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts "\t\t\t\t-----------"
+    puts "\t\t\t\t #{@board[6]} | #{@board[7]} | #{@board[8]}"
+    puts "\t\t\t******************************"
+  end
+
+  def game_status(player)
+    return true if check_horizontal(player) || check_vertical(player) || check_cross(player)
+
+    false
+  end
+
+  private
+
+  def mark(box, player)
+    @board[box] = player.sign
+  end
+
+  def check_horizontal(player)
+    i = 0
+    while i < 9
+      return true if check_hor(i, player)
+
+      i += 3
+    end
+    false
+  end
+
+  def check_hor(num, player)
+    return true if @board[num] == player.sign && @board[num + 1] == player.sign && @board[num + 2] == player.sign
+
+    false
+  end
+
+  def check_vertical(player)
+    i = 0
+    while i < 3
+      return true if check_vert(i, player)
+
+      i += 1
+    end
+
+    false
+  end
+
+  def check_vert(num, player)
+    return true if @board[num] == player.sign && @board[num + 3] == player.sign && @board[num + 6] == player.sign
+
+    false
+  end
+
+  def check_cross(player)
+    return true if left_diag(player) || right_diag(player)
+
+    false
+  end
+
+  def left_diag(player)
+    return true if @board[0] == player.sign && @board[4] == player.sign && @board[8] == player.sign
+
+    false
+  end
+
+  def right_diag(player)
+    return true if @board[2] == player.sign && @board[4] == player.sign && @board[6] == player.sign
+
+    false
+  end
+end
+
+# players
+class Player
+  attr_accessor :name, :sign
+  def initialize
+    @name = ''
+    @sign = ''
+  end
+end
